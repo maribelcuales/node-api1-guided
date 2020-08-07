156cf6c996bcde5e96590e88f10344350b2e5a89
maribelcuales <maribelcuales@gmail.com>
2020-08-07 15:42:57 +0800

    Added post request to add a new lesson.

--- index.js
+++ index.js
@@ -3,20 +3,32 @@ const express = require('express');  //CommonJS Modules
 
 const server = express();
 
+server.use(express.json());  // teaches express how to read JSON from the body 
+
+let lessons = [
+  {
+    id: 1, 
+    name: 'Introduction to HTTP APIs with Node and Express'
+  }
+];
+
 server.get('/', (req, res) => {
 	res.json({ api: "Up and running!" });
 });
 
 server.get('/api/lessons', function(req, res) {
   // return an array of lessons (id, name)
-  const lessons = [
-    {
-      id: 1, 
-      name: 'Introduction to HTTP APIs with Node and Express'
-    }
-  ];
+  
   res.json(lessons); 
 });
+
+server.post('/api/lessons', function(req, res) {
+  const lessonInformation = req.body;
+
+  lessons.push(lessonInformation);
+  
+  res.status(201).json(lessonInformation); 
+})
 
 server.listen(8000, () => console.log("\n== API is up ==\n")); 
 
