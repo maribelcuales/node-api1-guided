e44bab57bbfd3092108f1d70222f4386e57fe9c1
maribelcuales <maribelcuales@gmail.com>
2020-08-07 14:57:03 +0800

    Added get request to view all lessons.

--- index.js
+++ index.js
@@ -7,5 +7,16 @@ server.get('/', (req, res) => {
 	res.json({ api: "Up and running!" });
 });
 
+server.get('/api/lessons', function(req, res) {
+  // return an array of lessons (id, name)
+  const lessons = [
+    {
+      id: 1, 
+      name: 'Introduction to HTTP APIs with Node and Express'
+    }
+  ];
+  res.json(lessons); 
+});
+
 server.listen(8000, () => console.log("\n== API is up ==\n")); 
 
